d4b7ebee962c77bf30b39eb508eaa4bc780cf311
Christian Kyle Tanaya <120697139+cktanaya@users.noreply.github.com>
2023-05-05 14:05:55 +0800

    Add files via upload

---
+++ src/views/settings.vue
@@ -0,0 +1,158 @@
+<template>
+    <div>
+      <loggednav />
+      <form @submit.prevent="submitForm">
+        <label>Personal Information:</label>
+        <div>
+          <label for="full-name">Full Name:</label>
+          <input type="text" id="full-name" v-model="form.fullName" />
+        </div>
+        <div>
+          <label for="gender">Gender:</label>
+          <input type="text" id="gender" v-model="form.gender" />
+        </div>
+        <div>
+          <label for="account-number">Account Number:</label>
+          <input type="text" id="account-number" v-model="form.accountNumber" />
+        </div>
+        <div>
+          <label for="course">Course:</label>
+          <input type="text" id="course" v-model="form.course" />
+        </div>
+        <div>
+          <label for="telephone">Telephone Number:</label>
+          <input type="text" id="telephone" v-model="form.telephone" />
+        </div>
+      </form>
+  
+      <form @submit.prevent="submitForm">
+        <label>Account Details:</label>
+        <div>
+          <label for="email">Email:</label>
+          <input type="text" id="email" required v-model="form.email" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="text" id="password" required v-model="form.password" />
+        </div>
+        <div style="position: relative;">
+          <label for="email-recovery">Email Recovery:</label>
+          <input type="text" id="email-recovery" required v-model="form.emailRecovery" />
+          <button class="save-button" type="submit">Save</button>
+        </div>
+      </form>
+  
+      <div class="modal" v-if="showModal" @click.self="hideModal">
+        <div class="modal-content" @click.stop>
+          <p>Saved Changes!</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import loggednav from "@/views/loggednav.vue";
+  
+  export default {
+    name: "AddingArticles",
+    components: { loggednav },
+    data() {
+      return {
+        form: {
+          fullName: "",
+          gender: "",
+          accountNumber: "",
+          course: "",
+          telephone: "",
+          email: "",
+          password: "",
+          emailRecovery: "",
+        },
+        showModal: false,
+      };
+    },
+    methods: {
+      submitForm() {
+        console.log(this.form);
+        // Do something with the form data, e.g. submit to a server
+        this.showModal = true;
+      },
+      hideModal() {
+        this.showModal = false;
+      },
+    },
+  };
+  </script>
+
+<style scoped>
+  form {
+    max-width: 100%;
+    max-height: 100%;
+    margin: 1in;
+    background: rgb(42, 42, 42);
+    text-align: left;
+    padding: 50px;
+    border-radius: 10px;
+  }
+  
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  
+  input,
+  select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  
+  .save-button {
+      background: #ffdd00;
+      border: 0;
+      padding: 10px 20px;
+      margin-top: 20px;
+      color: rgb(0, 0, 0);
+      border-radius: 20px;
+      position: relative;
+      top: 20px;
+      left: 50%;
+      transform: translateX(-50%);
+  }
+  .modal {
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(53, 52, 52, 0.5);
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: #e0e0e0;
+  border-radius: 10px;
+  padding: 20px;
+  text-align: center;
+}
+
+.modal p {
+  color: #000000;
+  font-size: 22px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+  </style>
